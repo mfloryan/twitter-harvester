42bed0e047d5526be56e16ba182301d0916ae566
Marcin Floryan <marcin.floryan@gmail.com>
2013-03-09 10:46:50 +0000

    Twitter stream moved to its own module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var mongo = require('mongodb'),
-    oauth = require('oauth-client'),
-    nconf = require('nconf');
+    nconf = require('nconf'),
+    twitter = require('./twitter-stream')
 
 nconf.file({file: 'config.json'});
 
@@ -17,77 +17,10 @@ var db = (function() {
         nconf.get('mongo:server'),
         nconf.get('mongo:port'),
         {auto_reconnect: true});
+    console.log('Connected to Mongo');
     return new mongo.Db(nconf.get('mongo:database'), server, {safe:true});
 })();
 
-function TwitterFeed(tweetCallback) {
-    var pub = {};
-
-    var body = {
-        track: ''
-    };
-
-    var requestConfig = {};
-
-    pub.setOAuthDetails = function(config) {
-        requestConfig = {
-            port: 443,
-            host: 'stream.twitter.com',
-            https: true,
-            path: '/1.1/statuses/filter.json',
-            oauth_signature: (function(){
-                var consumer = oauth.createConsumer(config.consumerKey, config.consumerSecret);
-                var token = oauth.createToken(config.accessTokenKey, config.accessTokenSecret);
-                return oauth.createHmac(consumer, token)
-            }()),
-            method: 'POST',
-            body : body
-        }
-    };
-
-    var errorCounter = 0;
-
-    var handleError = function(error) {
-        errorCounter++;
-        console.log("Got an error: " + error);
-        if (errorCounter < 5) {
-            console.log("Will try again soon");
-            setTimeout(startTwitterFeed, 10000 * errorCounter);
-        } else {
-            console.log("Sorry. Too many errors. Bye!")
-        }
-    };
-
-    var handleChunk = function(chunk) {
-        if (chunk && chunk.trim()) {
-            var data = JSON.parse(chunk);
-            if (data && tweetCallback) tweetCallback(data);
-        } else {
-            console.log('.');
-        }
-    };
-
-    pub.start = function(itemToTrack) {
-        body.track = itemToTrack;
-        startTwitterFeed();
-    };
-
-    function startTwitterFeed() {
-        var request = oauth.request(requestConfig, function(response) {
-            console.log("Here is my response code: "+ response.statusCode);
-            console.log("Now waiting for some interesting Twitter data");
-            console.log();
-            response.setEncoding('utf8');
-            response.on('data', handleChunk);
-        });
-
-        request.write(body);
-        request.end();
-        request.on('error', handleError);
-    }
-
-    return pub;
-}
 
 var saveTweet = function(tweet) {
     if (tweet.id) {
@@ -100,12 +33,10 @@ var saveTweet = function(tweet) {
     console.log("Saved: " + tweet.id);
 };
 
-var twitter = new TwitterFeed(saveTweet);
+var feed = new twitter(saveTweet);
 
-twitter.setOAuthDetails(nconf.get('oauth'));
+feed.setOAuthDetails(nconf.get('oauth'));
 
 db.open(function() {
-    twitter.start("agile");
+    feed.start("agile");
 });
-
-db.close();
---
+++ twitter-stream.js
@@ -0,0 +1,74 @@
+var oauth = require('oauth-client');
+
+function twitterStream(tweetCallback) {
+
+    var pub = {};
+
+    var body = {
+        track: ''
+    };
+
+    var requestConfig = {};
+
+    pub.setOAuthDetails = function(config) {
+        requestConfig = {
+            port: 443,
+            host: 'stream.twitter.com',
+            https: true,
+            path: '/1.1/statuses/filter.json',
+            oauth_signature: (function(){
+                var consumer = oauth.createConsumer(config.consumerKey, config.consumerSecret);
+                var token = oauth.createToken(config.accessTokenKey, config.accessTokenSecret);
+                return oauth.createHmac(consumer, token)
+            }()),
+            method: 'POST',
+            body : body
+        }
+    };
+
+    var errorCounter = 0;
+
+    var handleError = function(error) {
+        errorCounter++;
+        console.log("Got an error: " + error);
+        if (errorCounter < 5) {
+            console.log("Will try again soon");
+            setTimeout(startTwitterFeed, 10000 * errorCounter);
+        } else {
+            console.log("Sorry. Too many errors. Bye!")
+        }
+    };
+
+    var handleChunk = function(chunk) {
+        if (chunk && chunk.trim()) {
+            var data = JSON.parse(chunk);
+            if (data && tweetCallback) tweetCallback(data);
+        } else {
+            console.log('.');
+        }
+    };
+
+    pub.start = function(itemToTrack) {
+        body.track = itemToTrack;
+        startTwitterFeed();
+    };
+
+    function startTwitterFeed() {
+        console.log("Connecting...");
+        var request = oauth.request(requestConfig, function(response) {
+            console.log("Here is my response code: "+ response.statusCode);
+            console.log("Now waiting for some interesting Twitter data");
+            console.log();
+            response.setEncoding('utf8');
+            response.on('data', handleChunk);
+        });
+
+        request.write(body);
+        request.end();
+        request.on('error', handleError);
+    }
+
+    return pub;
+}
+
+module.exports = twitterStream;
