082fdeaebfc67b130f9c6e4a5ecdb03496e19354
Marcin Floryan <marcin.floryan@gmail.com>
2013-03-10 18:47:55 +0000

    We can enrich two events now

--- spec/enrich-tweet-stream.spec.js
+++ spec/enrich-tweet-stream.spec.js
@@ -1,7 +1,7 @@
 var twitterEvents = require('../lib/understand-twitter-stream');
 
 describe("Enrich twitter events", function() {
-    it("when the event is friends list", function() {
+    it("can enrich friends list event", function() {
         var data = { "friends" : [
             370844675,
             100308547,
@@ -12,7 +12,25 @@ describe("Enrich twitter events", function() {
         var event = twitterEvents.transform(data);
 
         expect(event.content).toBe(data);
-        expect(event.type).toBe('friends');
-        expect(event.category).toBe('user');
+        expect(event.event).toBe('friends');
+        expect(event.subject).toBe('user');
     });
+
+    it ("can enrich delete event", function() {
+        var data = { "delete":
+            { "status": {
+                "id":3.105417376742851e+17,
+                "user_id":14374075,
+                "id_str":"310541737674285056",
+                "user_id_str":"14374075" }
+            }
+        };
+
+        var event = twitterEvents.transform(data);
+
+        expect(event.content).toBe(data);
+        expect(event.subject).toBe('tweet');
+        expect(event.event).toBe('delete');
+        expect(event.statusId).toBe(data.delete.status.id);
+    })
 });
